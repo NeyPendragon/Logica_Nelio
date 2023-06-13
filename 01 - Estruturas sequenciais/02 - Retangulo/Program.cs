ec44ee1cd47c29c0c6ed5b7918dac46b9637700a
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-12 21:52:09 -0300

    One more exercise
    Good commit =)

---
+++ 01 - Estruturas sequenciais/02 - Retangulo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Base do retângulo: ");
+        string baseRetanguloInput = Console.ReadLine();
+        Console.WriteLine("Altura do retângulo: ");
+        string alturaInput = Console.ReadLine();
+
+        double baseRetangulo, altura;
+
+        if (double.TryParse(baseRetanguloInput, out baseRetangulo) && double.TryParse(alturaInput, out altura))
+
+        {
+            double areaFinal = (baseRetangulo * altura);
+            Console.WriteLine("AREA = {0:F4}", areaFinal);
+            double perimetro = ((baseRetangulo + altura) * 2);
+            Console.WriteLine("PERÍMETRO = {0:F4}", perimetro);
+            double diagonal = Math.Sqrt(Math.Pow(baseRetangulo, 2) + Math.Pow(altura, 2));
+            Console.WriteLine("DIAGONAL = {0:F4}", diagonal);
+          
+        }
+
+        else
+        {
+            Console.WriteLine("Erro");
+        }
+    }
+}
