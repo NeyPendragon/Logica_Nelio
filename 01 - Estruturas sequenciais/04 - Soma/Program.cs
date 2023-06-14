a3c486bd23cb0a1a5acc823e3106bcf2c0cd66fb
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-14 19:29:53 -0300

    Uploading one exercise a day

---
+++ 01 - Estruturas sequenciais/04 - Soma/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    
+    Console.WriteLine ("Digite o valor de X: ");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o valor de Y: ");
+    double y = double.Parse(Console.ReadLine());
+
+    double soma = (x + y);
+
+    Console.WriteLine($"SOMA = {soma}");
+
+  }
+}
