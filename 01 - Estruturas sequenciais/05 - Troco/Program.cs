9ee3e1712b26e296ce65f03852353015bd12dd84
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-15 23:48:22 -0300

    Exercício Troco

---
+++ 01 - Estruturas sequenciais/05 - Troco/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    
+    Console.WriteLine ("Digite o valor do produto: ");
+    double valorUnidade = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite quantidade comprada: ");
+    double quantidade = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o valor pago: ");
+    double recebido = double.Parse(Console.ReadLine());
+
+    double valorTotal = (valorUnidade * quantidade);
+    double troco = recebido - valorTotal;
+
+    Console.WriteLine($"Preço unitário do produto: {valorUnidade:F2}\nQuantidade comprada: {quantidade}\nDinheiro recebido: {recebido:F2}\nTROCO: {troco:F2}");
+
+  }
+}
