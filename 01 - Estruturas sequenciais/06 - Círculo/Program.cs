f7e40dbe6fcf898b59b4bb11704c874399f00aa1
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-16 17:40:12 -0300

    Add files via upload

---
+++ 01 - Estruturas sequenciais/06 - Círculo/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Digite o valor do raio do circulo:");
+    double r = double.Parse(Console.ReadLine());
+
+    double area = Math.Pow(r, 2) * Math.PI;
+
+    Console.WriteLine ($"AREA: {area:F4}");
+  }
+}
