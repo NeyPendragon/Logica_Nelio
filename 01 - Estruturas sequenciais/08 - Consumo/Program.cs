b0c9531a07d96f0eeffd92b477d70d54390c9fe3
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-19 18:07:55 -0300

    Exercicio Consumo.

---
+++ 01 - Estruturas sequenciais/08 - Consumo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        double dist = ReadDouble("Distância: ");
+        double combGasto = ReadDouble("Combustível Gasto: ");
+
+        double media = dist / combGasto;
+
+        Console.WriteLine($"Distância Percorrida: {dist}");
+        Console.WriteLine($"Combustível gasto: {combGasto:F1}");
+        Console.WriteLine($"Consumo médio: {media:F3}");
+    }
+    private static double ReadDouble(String message)
+
+    {
+        while (true)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            if (double.TryParse(input, out double result))
+            {
+                return result;
+            }
+
+            Console.WriteLine("Digite um número válido.");
+
+        }
+    }
+}
