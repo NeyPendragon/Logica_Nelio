af1dcf535c3bd7800b5c03dbc646e2658126b826
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-21 20:51:30 -0300

    Mais um exercício básico resolvido.

---
+++ 01 - Estruturas sequenciais/10 - Duracao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) 
+  
+  {
+    Console.WriteLine("Digite a duração em segundos: ");
+    int segundos = int.Parse(Console.ReadLine());
+
+    int horas = segundos / 3600;
+    segundos = segundos % 3600;
+
+    int minutos = segundos / 60;
+    segundos = segundos % 60;
+
+    Console.WriteLine("\n" + horas.ToString("D2") + ":" + 
+minutos.ToString("D2") + ":" + segundos.ToString("D2"));
+  }
+} 
