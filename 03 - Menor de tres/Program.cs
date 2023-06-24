9de6996d2c2a4f20d19cddd55bbd979e6dbcc81e
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-24 18:57:30 -0300

    Menor de 3

---
+++ 03 - Menor de tres/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+
+{
+    public static void Main(string[] args)
+    {
+
+      int valorX, valorY, valorZ, menor;
+      
+      Console.WriteLine("Primeiro Valor: ");
+      valorX = int.Parse(Console.ReadLine());
+      Console.WriteLine("Segundo Valor: ");
+      valorY = int.Parse(Console.ReadLine());
+      Console.WriteLine("Terceiro Valor: ");
+      valorZ = int.Parse(Console.ReadLine());
+
+      if (valorX < valorY && valorX < valorZ)
+      {
+        menor = valorX;
+      }
+      
+      else if (valorY < valorZ)
+      {
+        menor = valorY;
+      }
+
+      else 
+      {
+        menor = valorZ;
+      }
+      Console.WriteLine("MENOR = {0}", menor);
+  }
+}
