9eec3cd2785812822fb732c5712b4839362099ab
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-26 19:31:49 -0300

    Novo problema resolvido

---
+++ 02 - Estruturas Condicionais/04 - Operadora/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program 
+{
+  public static void Main (string[] args) 
+  
+  {
+    int minutos, valor;
+    double minimo = 50.00;
+    
+    Console.WriteLine ("Digite a quantidade de minutos:");
+    minutos = int.Parse(Console.ReadLine());
+
+    if(minutos <= 100)
+    {
+      Console.WriteLine("Valor a pagar: {0:F2}", minimo);
+    }
+    else
+    {
+      valor = ((minutos - 100) * 2) + 50;
+      Console.WriteLine ("VALOR: {0:F2}", valor);
+    }
+  }
+}
