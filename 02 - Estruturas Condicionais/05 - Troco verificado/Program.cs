6b483b3d82c92fd8a40eac617b3b7396324ef35e
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-27 20:25:02 -0300

    Problema Troco Verificado

---
+++ 02 - Estruturas Condicionais/05 - Troco verificado/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program 
+{
+  public static void Main (string[] args) 
+  
+  {    
+
+    int quantidade;
+    double preco, recebido, troco, total;
+    
+    Console.WriteLine ("Preço unitário do produto");
+    preco = double.Parse(Console.ReadLine());
+    Console.WriteLine("Quantidade comprada: ");
+    quantidade = int.Parse(Console.ReadLine());
+    Console.WriteLine("Dinheiro recebido: ");
+    recebido = double.Parse(Console.ReadLine());
+
+    total = preco * quantidade;
+    
+    if (recebido < total)
+    {
+      troco = (total - recebido);
+      Console.WriteLine("DINHEIRO INSUFICIENTE. FALTAM {0:F2} Reais", troco);
+    }
+    else
+    {
+      troco = (recebido - total);
+      Console.WriteLine("Troco: {0:F2}", troco);
+    }
+  }
+}
