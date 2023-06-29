97b744ab307b6352fd30e3f33c0bdf1358cd0d13
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-28 22:03:01 -0300

    Problema Glicose

---
+++ 02 - Estruturas Condicionais/06 - Glicose/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program 
+{
+  public static void Main (string[] args) 
+  
+  {
+    double valor;
+
+    Console.WriteLine ("Digite a medida da glicose: ");
+    valor = double.Parse(Console.ReadLine());
+
+    if(valor < 100)
+    {
+      Console.WriteLine("Classificação: NORMAL");
+    }
+    else if(valor > 140)
+    {
+      Console.WriteLine("Classificação: DIABETES");
+    }
+    else
+    {
+      Console.WriteLine("Classificação: ELEVADO");
+    }
+  }
+}
