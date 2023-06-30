73c7bdae04e891953d03e4fe9128d45dabdaa1de
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-06-29 23:12:30 -0300

    Problema Dardo

---
+++ 02 - Estruturas Condicionais/07 - Dardo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program 
+{
+  public static void Main (string[] args) 
+  
+  {
+    double dist1, dist2, dist3;
+    
+    Console.WriteLine ("Digite as tres distâncias: ");
+    dist1 = double.Parse(Console.ReadLine());
+    Console.Write("");
+    dist2 = double.Parse(Console.ReadLine());
+    Console.Write("");
+    dist3 = double.Parse(Console.ReadLine());
+
+    if((dist3 > dist2) && (dist3 > dist1))
+    {
+      Console.WriteLine ("MAIOR DISTANCIA = {0:F2}", dist3);
+    }
+    else if(dist2 > dist1)
+    {
+      Console.WriteLine("MAIOR DISTANCIA = {0:F2}", dist2);
+    }
+    else
+    {
+      Console.WriteLine("MAIOR DISTANCIA = {0:F2}", dist1);
+    }
+  }
+}
