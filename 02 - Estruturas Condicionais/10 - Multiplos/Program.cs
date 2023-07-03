723325d0aa23f3040350c02b6359eeb7a2a52c55
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-03 20:02:49 -0300

    Exercício Múltiplos

---
+++ 02 - Estruturas Condicionais/10 - Multiplos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        int firstNumber = GetUserInput("Digite o primeiro número inteiro:");
+        int secondNumber = GetUserInput("Digite o segundo número inteiro:");
+
+        if (AreMultiples(firstNumber, secondNumber))
+        {
+            Console.WriteLine("São múltiplos.");
+        }
+        else 
+        {
+            Console.WriteLine("Não são múltiplos.");
+        }
+    }
+
+    static int GetUserInput(string message)
+    {
+        Console.WriteLine(message);
+        int number;
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro válido:");
+        }
+        return number;
+    }
+
+    static bool AreMultiples(int num1, int num2)
+    {
+        return num1 % num2 == 0 || num2 % num1 == 0;
+    }
+}
