75306e2e17b6e1710fcfef349f2f18ca2b7df065
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-05 22:23:51 -0300

    Tempo de jogo

---
+++ 02 - Estruturas Condicionais/12 - Tempo de Jogo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite a hora inicial do jogo:");
+        int horaInicial = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a hora final do jogo:");
+        int horaFinal = int.Parse(Console.ReadLine());
+
+        int duracao = CalcularDuracaoDoJogo(horaInicial, horaFinal);
+
+        Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+    }
+
+    public static int CalcularDuracaoDoJogo(int horaInicial, int horaFinal)
+    {
+        int duracao;
+
+        if (horaInicial < horaFinal)
+        {
+            duracao = horaFinal - horaInicial;
+        }
+        else
+        {
+            duracao = 24 - horaInicial + horaFinal;
+        }
+
+        return duracao;
+    }
+}
