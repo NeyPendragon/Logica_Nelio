ea593d810b3f53080d5c4f3f662ff8c1ceb3d096
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-06 21:05:57 -0300

    Problema Coordenadas

---
+++ 02 - Estruturas Condicionais/13 - Coordenadas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Valor de X: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Valor de Y: ");
+        double y = double.Parse(Console.ReadLine());
+
+        if (x == 0 && y == 0)
+        {
+            Console.WriteLine("Origem");
+        }
+        else if (x == 0)
+        {
+            Console.WriteLine("Eixo Y");
+        }
+        else if (y == 0)
+        {
+            Console.WriteLine("Eixo X");
+        }
+        else if (x > 0 && y > 0)
+        {
+            Console.WriteLine("Q1");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("Q2");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("Q3");
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine("Q4");
+        }
+    }
+}
