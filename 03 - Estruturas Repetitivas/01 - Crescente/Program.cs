6090f6ef0f552aa5e293abbcebbc2cd1e7db2319
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-07 18:49:33 -0300

    Problema Crescente

---
+++ 03 - Estruturas Repetitivas/01 - Crescente/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite dois números:");
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        while (x != y)
+        {
+            if (x < y)
+            {
+                Console.WriteLine("CRESCENTE!");
+            }
+            else
+            {
+                Console.WriteLine("DECRESCENTE!");
+            }
+
+            Console.WriteLine("Digite outros dois números:");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Números iguais. Encerrando o programa.");
+    }
+}
