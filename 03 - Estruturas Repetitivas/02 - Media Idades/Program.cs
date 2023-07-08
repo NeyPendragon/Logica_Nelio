bc02194fc7a24579db71f358b2eb766c6dc8c1e0
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-08 20:06:41 -0300

    PRoblema Media Idades

---
+++ 03 - Estruturas Repetitivas/02 - Media Idades/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite as idades:");
+
+        var calculadoraMedia = new CalculadoraMedia();
+
+        while (true)
+        {
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade < 0)
+                break;
+
+            calculadoraMedia.AdicionarIdade(idade);
+        }
+
+        double media = calculadoraMedia.CalcularMedia();
+
+        if (media >= 0)
+            Console.WriteLine("MEDIA = " + media.ToString("F2"));
+        else
+            Console.WriteLine("IMPOSSIVEL CALCULAR");
+    }
+}
+
+class CalculadoraMedia
+{
+    private int somaIdades;
+    private int quantidadeIdades;
+
+    public void AdicionarIdade(int idade)
+    {
+        somaIdades += idade;
+        quantidadeIdades++;
+    }
+
+    public double CalcularMedia()
+    {
+        if (quantidadeIdades == 0)
+            return -1;
+
+        return (double)somaIdades / quantidadeIdades;
+    }
+}
