acf53d46cadcd7ea1cafa26184eacaf41f061b2c
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-10 20:23:49 -0300

    Add files via upload

---
+++ 03 - Estruturas Repetitivas/03 - Senha fixa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int senhaCorreta = 2002;
+        
+        Console.WriteLine("Digite a senha:");
+        
+        while (true)
+        {
+            int senhaDigitada = int.Parse(Console.ReadLine());
+            
+            if (senhaDigitada == senhaCorreta)
+            {
+                Console.WriteLine("Acesso permitido!");
+                break;
+            }
+            
+            Console.WriteLine("Senha inválida! Tente novamente:");
+        }
+    }
+}
