616536e87af414eedf0afcb10f9bdd410cd8f84b
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-12 20:36:08 -0300

    Problema Validação de nota

---
+++ 03 - Estruturas Repetitivas/05 - Validacao de Nota/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class ValidadorDeNota
+{
+    static void Main(string[] args)
+    {
+        double primeiraNota = 0, segundaNota = 0;
+        bool primeiraNotaValida = false, segundaNotaValida = false;
+
+        while (!primeiraNotaValida)
+        {
+            Console.Write("Digite a primeira nota: ");
+            if (double.TryParse(Console.ReadLine(), out primeiraNota))
+            {
+                if (primeiraNota >= 0 && primeiraNota <= 10)
+                {
+                    primeiraNotaValida = true;
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido! Tente novamente.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido! Tente novamente.");
+            }
+        }
+
+        while (!segundaNotaValida)
+        {
+            Console.Write("Digite a segunda nota: ");
+            if (double.TryParse(Console.ReadLine(), out segundaNota))
+            {
+                if (segundaNota >= 0 && segundaNota <= 10)
+                {
+                    segundaNotaValida = true;
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido! Tente novamente.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido! Tente novamente.");
+            }
+        }
+
+        double media = (primeiraNota + segundaNota) / 2;
+        Console.WriteLine("MEDIA = " + media.ToString("F2"));
+    }
+}
