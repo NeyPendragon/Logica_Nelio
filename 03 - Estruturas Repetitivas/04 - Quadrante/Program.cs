66c01a20741b341f7cc8748583105ec26ca32e7b
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-11 21:12:59 -0300

    Problema Quadrante
    Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence (Q1, Q2, Q3 ou Q4). O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
    
    Exemplo:
    Digite os valores das coordenadas X e Y:
    2
    2
    QUADRANTE Q1
    Digite os valores das coordenadas X e Y:
    3
    -2
    QUADRANTE Q4
    Digite os valores das coordenadas X e Y:
    -8
    -1
    QUADRANTE Q3
    Digite os valores das coordenadas X e Y:
    -7
    1
    QUADRANTE Q2
    Digite os valores das coordenadas X e Y:
    0
    2

---
+++ 03 - Estruturas Repetitivas/04 - Quadrante/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Ponto
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Ponto(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public string ObterQuadrante()
+    {
+        if (X > 0 && Y > 0)
+        {
+            return "Q1";
+        }
+        else if (X < 0 && Y > 0)
+        {
+            return "Q2";
+        }
+        else if (X < 0 && Y < 0)
+        {
+            return "Q3";
+        }
+        else // única resposta possível: X > 0 && Y < 0
+        {
+            return "Q4";
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Digite os valores das coordenadas X e Y:");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x == 0 || y == 0)
+            {
+                break;
+            }
+
+            Ponto ponto = new Ponto(x, y);
+            string quadrante = ponto.ObterQuadrante();
+            Console.WriteLine($"QUADRANTE {quadrante}");
+        }
+    }
+}
