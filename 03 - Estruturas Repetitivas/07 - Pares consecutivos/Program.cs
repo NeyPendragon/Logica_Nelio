ba70792d6dab6bf36522a86341ccf91b7a3e78d8
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-14 18:47:27 -0300

    Problema Pares consecutivos

---
+++ 03 - Estruturas Repetitivas/07 - Pares consecutivos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int x;
+
+        do
+        {
+            Console.Write("Digite um número inteiro: ");
+            x = int.Parse(Console.ReadLine());
+
+            if (x != 0)
+            {
+                int sum = CalculateSum(x);
+                Console.WriteLine($"SOMA = {sum}");
+            }
+        }
+        while (x != 0);
+    }
+
+    public static int CalculateSum(int x)
+    {
+        int sum = 0;
+
+        if (x % 2 != 0)
+        {
+            x++; // Increment x to the next even number
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            sum += x;
+            x += 2;
+        }
+
+        return sum;
+    }
+}
