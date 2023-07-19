eec07fd775ddd6bb79a71a084ec3268b7f97e46b
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-18 22:17:42 -0300

    Problema Soma impares

---
+++ 03 - Estruturas Repetitivas/09 - Soma Impares/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite dois numeros:");
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        int start = Math.Min(x, y);
+        int end = Math.Max(x, y);
+
+        for (int i = start + 1; i < end; i++)
+        {
+            if (i % 2 != 0)
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine("SOMA DOS IMPARES = " + sum);
+    }
+}
