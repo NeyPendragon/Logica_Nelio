0197cab546f6e2240f3e15c6b8137f63c4d295df
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-20 18:15:50 -0300

    Problema Dentro fora

---
+++ 03 - Estruturas Repetitivas/11 - Dentro fora/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DentroFora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos numeros voce vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int dentroIntervalo = 0;
+            int foraIntervalo = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite um numero: ");
+                int x = int.Parse(Console.ReadLine());
+
+                if (x >= 10 && x <= 20)
+                {
+                    dentroIntervalo++;
+                }
+                else
+                {
+                    foraIntervalo++;
+                }
+            }
+
+            Console.WriteLine($"{dentroIntervalo} DENTRO");
+            Console.WriteLine($"{foraIntervalo} FORA");
+        }
+    }
+}
