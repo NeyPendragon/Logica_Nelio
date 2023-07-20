84a66e094e5dc551ba0fddebd130ae7eee43b0f0
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-19 21:17:15 -0300

    Problema sequancia impares

---
+++ 03 - Estruturas Repetitivas/10 - Sequencia impares/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SequenciaImpares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de X: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Números ímpares de 1 até X:");
+
+            for (int i = 1; i <= x; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
