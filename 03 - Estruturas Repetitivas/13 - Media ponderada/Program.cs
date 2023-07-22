d9b169f343a2b5029e3cde780d0698dbf7e53355
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-22 19:26:05 -0300

    Problema media ponderada

---
+++ 03 - Estruturas Repetitivas/13 - Media ponderada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MediaPonderada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos casos você vai digitar? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Digite três números: ");
+                double num1 = double.Parse(Console.ReadLine());
+                double num2 = double.Parse(Console.ReadLine());
+                double num3 = double.Parse(Console.ReadLine());
+
+                double mediaPonderada = (num1 * 2 + num2 * 3 + num3 * 5) / 10;
+
+                Console.WriteLine($"MEDIA = {mediaPonderada.ToString("F1")}");
+            }
+        }
+    }
+}
