bc266925c8c61d5afb786008dca6ce5825c7fb19
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-21 21:19:16 -0300

    Problema par impar

---
+++ 03 - Estruturas Repetitivas/12 - Par impar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Quantos números você vai digitar? ");
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero == 0)
+            {
+                Console.WriteLine("NULO");
+            }
+            else
+            {
+                if (numero % 2 == 0)
+                {
+                    Console.Write("PAR ");
+                }
+                else
+                {
+                    Console.Write("IMPAR ");
+                }
+
+                if (numero > 0)
+                {
+                    Console.WriteLine("POSITIVO");
+                }
+                else
+                {
+                    Console.WriteLine("NEGATIVO");
+                }
+            }
+        }
+    }
+}
