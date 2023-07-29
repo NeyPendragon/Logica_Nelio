b51946dfdf235a2179c8541a8447f58c12fc42d0
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-28 22:26:07 -0300

    Nova pasta e exercício
    Cria pasta para os exercícios de vetores e cria o primeiro exercício.

---
+++ 04 - Vetores/01 - Negativos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumerosNegativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos numeros voce vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numeros = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite um numero: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("NUMEROS NEGATIVOS:");
+            foreach (int numero in numeros)
+            {
+                if (numero < 0)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+        }
+    }
+}
