2b13d62a6ff112fbf3930232dd37bc95ac8370f7
Ney Victor <99369083+NeyPendragon@users.noreply.github.com>
2023-07-31 23:06:23 -0300

    Problema Soma vetor

---
+++ 04 - Vetores/02 - Soma vetor/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SomaVetor
+{
+    class Vetor
+    {
+        private double[] numeros;
+
+        public Vetor(int tamanho)
+        {
+            numeros = new double[tamanho];
+        }
+
+        public void LerNumeros()
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Digite um numero: ");
+                numeros[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        public void ImprimirValores()
+        {
+            Console.Write("VALORES = ");
+            foreach (double numero in numeros)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public double CalcularSoma()
+        {
+            double soma = 0;
+            foreach (double numero in numeros)
+            {
+                soma += numero;
+            }
+            return soma;
+        }
+
+        public double CalcularMedia()
+        {
+            double soma = CalcularSoma();
+            return soma / numeros.Length;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos numeros voce vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Vetor vetor = new Vetor(n);
+            vetor.LerNumeros();
+
+            vetor.ImprimirValores();
+
+            double soma = vetor.CalcularSoma();
+            Console.WriteLine($"SOMA = {soma:F2}");
+
+            double media = vetor.CalcularMedia();
+            Console.WriteLine($"MEDIA = {media:F2}");
+        }
+    }
+}
